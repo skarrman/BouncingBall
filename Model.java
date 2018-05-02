e7b6ec0d45aa67f021a61d08dc6bc9b53b55aa60
skarrman <simonkarrman@icloud.com>
2018-05-02 12:37:47 +0200

    A start in the calc for v

--- Model.java
+++ Model.java
@@ -23,8 +23,8 @@ class Model {
 		
 		// Initialize the model with a few balls
 		balls = new Ball[2];
-		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.2);
-		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3);
+		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.2, 2);
+		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3, 3);
 	}
 
 	void step(double deltaT) {
@@ -39,8 +39,18 @@ class Model {
 			}
 			for(Ball b2 : balls){
 				if (b2 != b) {
-					if(isCollision(b, b2))
+					if(isCollision(b, b2)) {
 						System.out.println("Bounce!");
+						double u1 = getVelocity(b.vx, b.vy);
+						double u2 = getVelocity(b2.vx, b2.vy);
+						double r = R(u1, u2);
+						double i = I(b.mass, u1, b2.mass, u2);
+						double v2 = v2(b.mass, b2.mass, r, i);
+						double v1 = v1(v2, r);
+
+
+//						break;
+					}
 				}
 			}
 			b.vy -= g * deltaT;
@@ -50,12 +60,17 @@ class Model {
 		}
 	}
 
-	//Fix the v1 hello you 
+	
 
-	double v2(double m1, double u1, double m2, double u2){
-		double r = R(u1,u2);
-		double i = I(m1,u1,m2,u2);
+	double getVelocity(double vx, double vy){
+		return Math.sqrt(vx * vx + vy * vy);
+	}
 
+	double v1(double v2, double r){
+		return v2-r;
+	}
+
+	double v2(double m1, double m2, double r, double i){
 		return (r*m1 + i) / (m1-m2);
 	}
 
@@ -67,8 +82,6 @@ class Model {
 		return m1 * u1 + m2 * u2;
 	}
 
-
-
 	boolean isCollision(Ball b1, Ball b2){
 		return Math.abs(Point.distance(b1.x,b1.y,b2.x,b2.y)) < (b1.radius + b2.radius);
 	}
@@ -78,17 +91,18 @@ class Model {
 	 */
 	class Ball {
 		
-		Ball(double x, double y, double vx, double vy, double r) {
+		Ball(double x, double y, double vx, double vy, double r, double mass) {
 			this.x = x;
 			this.y = y;
 			this.vx = vx;
 			this.vy = vy;
 			this.radius = r;
+			this.mass = mass;
 		}
 
 		/**
 		 * Position, speed, and radius of the ball. You may wish to add other attributes.
 		 */
-		double x, y, vx, vy, radius;
+		double x, y, vx, vy, radius, mass;
 	}
 }
