ef989a90ed2ddd02afe5c4dd84d93f44a0567636
skarrman <simonkarrman@icloud.com>
2018-05-16 10:59:25 +0200

    Started using matrices and vectors

--- Model.java
+++ Model.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.Vector;
 
 /**
  * The physics model.
@@ -24,8 +25,8 @@ class Model {
 		
 		// Initialize the model with a few balls
 		balls = new Ball[2];
-		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.3, 20, Color.red);
-				balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3, 20, Color.green);
+		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 0, 0.3, 20, Color.red);
+				balls[1] = new Ball(2 * width / 3, height * 0.9, -1, 0, 0.3, 20, Color.green);
 	}
 
 	void step(double deltaT) {
@@ -34,63 +35,22 @@ class Model {
 		if (time-- <= 0 && isCollision(balls[0], balls[1])) {
 //			System.out.println("Bounce!");
 
-			Ball firstBall;
-			Ball secondBall;
+			Ball ball1 = balls[0];
+			Ball ball2 = balls[1];
 
-			if(balls[0].y < balls[1].y) {
-				firstBall = balls[0];
-				secondBall = balls[1];
-			}else {
-				firstBall = balls[1];
-				secondBall = balls[0];
-			}
+			Vector v1 = new Vector(ball1.vx, ball1.vy);
+			Vector v2 = new Vector(ball2.vx, ball2.vy);
 
+			double a1 = getAngle(0,0, ball1.vx, ball1.vy);
+			double a2 = getAngle(0,0, ball2.vx, ball2.vy);
 
-			double vp1 = getVelocity(firstBall.vx, firstBall.vy);
-			double vp2 = getVelocity(secondBall.vx, secondBall.vy);
+			double b1 = getAngle(ball1.x,ball1.y, ball2.x, ball2.y);
+			double b2 = getAngle(ball2.x,ball2.y, ball1.x, ball1.y);
 
-			double a1 = getAngle(firstBall.vx, firstBall.vy, 0, 0);
-			double a2 = getAngle(secondBall.vx, secondBall.vy, 0, 0);
+			v1 = rotate(v1, b1-a1);
+			v2 = rotate(v2, b2-a2);
 
-			double b1 = getAngle(firstBall.x, firstBall.y, secondBall.x, secondBall.y);
-			double b2 = getAngle(secondBall.x, secondBall.y, firstBall.x, firstBall.y) - Math.PI;
-
-			double t1 = b1 - a1;
-			double t2 = b2 - a2;
-
-			double ux1 = getXVelocity(t1, vp1);
-			double ux2 = getXVelocity(t2, vp2);
-			double uy1 = getYVelocity(t1, vp1);
-			double uy2 = getYVelocity(t2, vp2);
-
-			System.out.println(firstBall.mass+", "+vp1+", "+ux1+", "+t1);
-			System.out.println(secondBall.mass+", "+vp2+", "+ux2+", "+t2);
-
-			double r = R(ux1, ux2);
-			double i = I(firstBall.mass, ux1, secondBall.mass, ux2);
-
-			double vx1 = v1(firstBall.mass, secondBall.mass, r, i);
-			double vx2 = v2(vx1, r);
-
-			double v1 = getVelocity(vx1, uy1);
-			double v2 = getVelocity(vx2, uy2);
-
-			System.out.println(v1);
-			System.out.println(v2);
-
-			double q1 = getAngle(vx1, uy1,0,0);
-			double q2 = getAngle(vx2, uy2,0,0);
-
-			double n1 = q1 + a1;
-			double n2 = q2 + a2 + Math.PI;
-
-			firstBall.vx = getXVelocity(n1, v1);
-			firstBall.vy = getYVelocity(n1, v1);
-
-			secondBall.vx = getXVelocity(n2, v2);
-			secondBall.vy = getYVelocity(n2, v2);
-
-			time = 20;
+			
 		}
 
 		for (Ball b : balls) {
@@ -101,12 +61,16 @@ class Model {
 			else if(b.y < b.radius && b.vy < 0 || b.y > areaHeight - b.radius && b.vy > 0) {
 				b.vy *= -1;
 			}else{
-				b.vy -= GRAVITY * deltaT;
+				//b.vy -= GRAVITY * deltaT;
 			}
 			// compute new position according to the speed of the ball
 			b.x += deltaT * b.vx;
 			b.y += deltaT * b.vy;
 		}
+	}
+
+	Vector rotate(Vector vector, double angle){
+		return new Vector(vector.x*Math.cos(angle) - vector.y*Math.sin(angle), vector.x*Math.sin(angle) + vector.y*Math.cos(angle));
 	}
 
 	double getYVelocity(double angle, double velocity){
@@ -146,6 +110,15 @@ class Model {
 
 	boolean isCollision(Ball b1, Ball b2){
 		return Math.abs(Point.distance(b1.x,b1.y,b2.x,b2.y)) <= (b1.radius + b2.radius) * 1.1;
+	}
+
+	private class Vector {
+		double x,y;
+
+		Vector(double x, double y){
+			this.x = x;
+			this.y = y;
+		}
 	}
 	
 	/**
